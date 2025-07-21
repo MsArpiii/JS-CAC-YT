ea4980164c5ceb5ccd8c950ad643f12a47630889
MsArpiii <arpitadas2115@gmail.com>
2025-07-21 06:35:55 +0000

    changes in variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,20 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
+let accountEmail = "arpita@google.com"
+var accountPassword = "abc123"
+accountCity = "Bhubaneswar"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountEmail = "ad@yahoo.com"
+accountPassword = "abcd1234"
+accountCity = "Cuttack"
 
 console.log(accountId);
 
 /*
-Prefer not to use var
-because of issue in block scope and functional scope
+Prefer not to use var because of the issue in both block and functional scope.
 */
-
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
